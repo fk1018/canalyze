9b1cca287fc81a69d2d1204ba0068785b62b99d8
fk1018 <fk1018@users.noreply.github.com>
2024-10-31 17:24:58 -0500

    Add CAN emulator framework
    Introduced a CAN emulator framework with base and specific vehicle classes. Added configuration for available emulators, including one for Mazda RX-8. Updated README to describe project purpose.

---
+++ can_emulator/lib/emulator.rb
@@ -0,0 +1,14 @@
+class Emulator
+  def initialize
+    raise "Emulator is an abstract base class and cannot be instantiated directly."
+  end
+
+  # Define any shared methods here, if needed
+  def start
+    puts 'Starting Emulator'
+  end
+
+  def send_can_message
+    puts 'Sending Can Message'
+  end
+end
